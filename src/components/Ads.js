636934c5415760446d1e22a0b1a9a70d79ed49b8
SparksFullStack <csparks@antioch.edu>
2019-03-21 17:03:35 -0700

    made changes to the ads section

--- src/components/Ads.js
+++ src/components/Ads.js
@@ -7,7 +7,7 @@ class Ads extends Component {
         <section id="boxes" class="py-5">
         <div className="container">
           <div className="row">
-            <div className="col-md-3">
+            <div className="col-md-4 card--container">
               <div className="card text-center border-primary mb-resp">
                 <div className="card-body">
                   <h3 className="text-primary">Ads go here</h3>
@@ -16,7 +16,7 @@ class Ads extends Component {
               </div>
             </div>
       
-            <div className="col-md-3">
+            <div className="col-md-4 card--container">
               <div className="card text-center bg-primary text-white mb-resp">
                 <div className="card-body">
                   <h3>Ads go here</h3>
@@ -25,7 +25,7 @@ class Ads extends Component {
               </div>
             </div>
       
-            <div className="col-md-3">
+            <div className="col-md-4">
               <div className="card text-center border-primary mb-resp">
                 <div className="card-body">
                   <h3 className="text-primary">Ads go here</h3>
@@ -34,14 +34,14 @@ class Ads extends Component {
               </div>
             </div>
       
-            <div className="col-md-3">
+            {/* <div className="col-md-3">
               <div className="card text-center bg-primary text-white">
                 <div className="card-body">
                   <h3>Ads go here</h3>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, laudantium?</p>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
--- src/components/Products.js
+++ src/components/Products.js
@@ -10,10 +10,11 @@ class Products extends Component {
             <div className="container">
                 <div className="row">
                     <div className="col">
-                        <div className="info-header mb-5">
-                        <h1 className="pb-3 products--header">
-                            Skins
+                    <div class="products--header mb-5">
+                        <h1 class="text-primary pb-3">
+                            Premium Skins
                         </h1>
+                        <p class="lead pb-3">The best skins on the market</p>
                         </div>
                     </div>
                 </div>
